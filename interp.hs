ff22aa73882bada3fe88841c3c2e6cce9e3fdaab
Robert <rcummings000@gmail.com>
2017-05-27 18:30:14 -0400

    Rework interp to use continuations

--- interp.hs
+++ interp.hs
@@ -33,32 +33,39 @@ data InterpError = NotANumber | BadDefine | Undefined
                  | NotAFunction | OtherInterpError | BadLoc deriving Show
 
 interpNum :: Ast -> State -> Either InterpError (Integer,State)
-interpNum x (e,s) = (interp x e s) >>= extract
+interpNum x (e,s) = (interp x e s EmptyCont) >>= extract
  where extract (Numb y,s') = Right (y,(e,s'))
        extract _ = Left NotANumber
 
 interpDefine :: String -> Ast -> State -> Either InterpError State
-interpDefine x a (e,s) = (interp a e s) >>= addtostate
+interpDefine x a (e,s) = (interp a e s EmptyCont) >>= addtostate
   where addtostate (v,s') = let l = newloc s' in Right ((x,l):e,(l,Found v):s')
 
 newloc :: Store -> Loc
 newloc = toInteger . length
 
-interp :: Ast -> Env -> Store -> Either InterpError (Val,Store)
-interp (Number v) _ s = Right (Numb v,s)
-interp (Fun p b) e s = Right (Closure p b e,s)
-interp (Bin op x y) e s = (interp x e s) >>= takeleft
-  where takeleft (Numb v,s') = (interp y e s') >>= takeright v
-        takeleft _ = Left LeftOperandNotNumber
-        takeright lv (Numb w,s'') = Right (Numb (opTrans op lv w),s'')
-        takeright _  _ = Left RightOperandNotNumber
-interp (App f x) e s = (interp f e s) >>= takefun
-  where takefun (Closure fp fb fe,s')
-          = let l = newloc s' in interp fb ((fp,l):fe) ((l,Todo x e):s')
-        takefun _ =  Left NotAFunction
-interp (Var x) e s = maybe (Left Undefined) takel (lookup x e)
+data InterpCont = EmptyCont | BinL Op Ast Env InterpCont | BinR Op Val InterpCont
+                | AppL Ast Env InterpCont | Memoize Loc InterpCont
+
+interp :: Ast -> Env -> Store -> InterpCont -> Either InterpError (Val,Store)
+interp (Number v) _ s c = applyInterpCont c (Numb v) s
+interp (Fun p b) e s c = applyInterpCont c (Closure p b e) s
+interp (Bin op x y) e s c = interp x e s (BinL op y e c)
+interp (App f x) e s c = interp f e s (AppL x e c)
+interp (Var x) e s c = maybe (Left Undefined) takel (lookup x e)
   where takel l = maybe (Left BadLoc) (takemem l) (lookup l s)
-        takemem l (Found y) = Right (y,s)
-        takemem l (Todo ast le) = (interp ast le s) >>= memoize l
-        memoize l (z,s') = Right (z,(l,Found z):s')
-interp (Define _ _) _ _ = Left BadDefine
+        takemem l (Found y) = applyInterpCont c y s
+        takemem l (Todo ast le) = interp ast le s (Memoize l c)
+interp (Define _ _) _ _ _ = Left BadDefine
+
+applyInterpCont :: InterpCont -> Val -> Store -> Either InterpError (Val,Store)
+applyInterpCont EmptyCont x s = Right (x,s)
+applyInterpCont (BinL op y e c) x@(Numb v) s = interp y e s (BinR op x c)
+applyInterpCont (BinL _ _ _ _) _ _ = Left LeftOperandNotNumber
+applyInterpCont (BinR op (Numb x) c) (Numb y) s
+  = applyInterpCont c (Numb (opTrans op x y)) s
+applyInterpCont (BinR _ _ _) _ _ = Left RightOperandNotNumber
+applyInterpCont (AppL x e c) (Closure fp fb fe) s
+  = let l = newloc s in interp fb ((fp,l):fe) ((l,Todo x e):s) c
+applyInterpCont (AppL _ _ _) _ _ = Left NotAFunction
+applyInterpCont (Memoize l c) x s = applyInterpCont c x ((l,Found x):s)
