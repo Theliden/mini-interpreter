d9b8bad81c0fe10f4a3d12332398f8cadf54e6e3
Robert <rcummings000@gmail.com>
2017-05-27 17:29:12 -0400

    More small changes to shorten error-handling code

--- interp.hs
+++ interp.hs
@@ -33,17 +33,13 @@ data InterpError = NotANumber | BadDefine | Undefined
                  | NotAFunction | OtherInterpError | BadLoc deriving Show
 
 interpNum :: Ast -> State -> Either InterpError (Integer,State)
-interpNum x (e,s) = case interp x e s of
-                    Right (Numb y,s') -> Right (y,(e,s'))
-                    Right _ -> Left NotANumber
-                    Left er -> Left er
+interpNum x (e,s) = (interp x e s) >>= extract
+ where extract (Numb y,s') = Right (y,(e,s'))
+       extract _ = Left NotANumber
 
 interpDefine :: String -> Ast -> State -> Either InterpError State
-interpDefine x a (e,s)
-  = case interp a e s of
-      Left er -> Left er
-      Right (v,s') -> let l = newloc s'
-                         in Right ((x,l):e,(l,Found v):s')
+interpDefine x a (e,s) = (interp a e s) >>= addtostate
+  where addtostate (v,s') = let l = newloc s' in Right ((x,l):e,(l,Found v):s')
 
 newloc :: Store -> Loc
 newloc = toInteger . length
@@ -51,15 +47,15 @@ newloc = toInteger . length
 interp :: Ast -> Env -> Store -> Either InterpError (Val,Store)
 interp (Number v) _ s = Right (Numb v,s)
 interp (Fun p b) e s = Right (Closure p b e,s)
-interp (Bin op x y) e s = (interp x e s) >>= getl
-  where getl (Numb v,s') = (interp y e s') >>= getr v
-        getl _ = Left LeftOperandNotNumber
-        getr v (Numb w,s'') = Right (Numb (opTrans op v w),s'')
-        getr _  _ = Left RightOperandNotNumber
-interp (App f x) e s = (interp f e s) >>= getf
-  where getf (Closure fp fb fe,s')
+interp (Bin op x y) e s = (interp x e s) >>= takeleft
+  where takeleft (Numb v,s') = (interp y e s') >>= takeright v
+        takeleft _ = Left LeftOperandNotNumber
+        takeright lv (Numb w,s'') = Right (Numb (opTrans op lv w),s'')
+        takeright _  _ = Left RightOperandNotNumber
+interp (App f x) e s = (interp f e s) >>= takefun
+  where takefun (Closure fp fb fe,s')
           = let l = newloc s' in interp fb ((fp,l):fe) ((l,Todo x e):s')
-        getf _ =  Left NotAFunction
+        takefun _ =  Left NotAFunction
 interp (Var x) e s = maybe (Left Undefined) takel (lookup x e)
   where takel l = maybe (Left BadLoc) (takemem l) (lookup l s)
         takemem l (Found y) = Right (y,s)
